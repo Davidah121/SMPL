0a745d071432e58f9eca4c8d8e67f7f7b375c6b3
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2025-04-18 00:28:07 -0400

    A few more files being cleaned up

--- code.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageSequence
-
-with Image.open("apng_test.png") as im:
-    index = 1
-    for frame in ImageSequence.Iterator(im):
-        frame.save(f"frame{index}.png")
-        index += 1
